92201c82dfad650ff27468afcc1c980447201009
Jante Haa <jante.haavanoksa@gmail.com>
2025-04-08 20:12:41 +0300

    App bar added with navigation menu

--- PersonalTrainerApp/src/App.tsx
+++ PersonalTrainerApp/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState } from "react";
 import CustomerList from "./CustomerList"
 import TrainingList from './TrainingList'
 
@@ -8,41 +9,92 @@ import AppBar from "@mui/material/AppBar"
 import Toolbar from "@mui/material/Toolbar"
 import Typography from "@mui/material/Typography"
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+const drawerWidth = 240;
+const navItems = ['Home', 'Trainings', 'Calendar'];
 
 function App() {
+  const [mobileOpen, setMobileOpen] = useState(false);
 
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={() => setMobileOpen(false)} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        Navigation
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
-    <Container maxWidth="xl">
+    <>
 
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
+      <Box sx={{ display: 'flex' }}>
+        <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
           <Toolbar>
             <IconButton
-              size="large"
-              edge="start"
               color="inherit"
-              aria-label="menu"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
               sx={{ mr: 2 }}
             >
               <MenuIcon />
             </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Typography variant="h6" noWrap component="div">
               Personal Trainer App
             </Typography>
-            <Button color="inherit">Login</Button>
           </Toolbar>
         </AppBar>
+
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
+          sx={{
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            mt: 8,
+            width: '100%',
+          }}
+        >
+          <Container maxWidth="xl">
+            <CustomerList />
+            <TrainingList />
+          </Container>
+        </Box>
       </Box>
-
-      <CustomerList />
-      <TrainingList />
-
-    </Container>
-  )
+    </>
+  );
 }
-
 export default App
